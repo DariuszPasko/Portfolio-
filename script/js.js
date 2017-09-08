5484e41084c945a73b4e4fa3c4e87a9860850f1a
Dariusz Paśko <dariuszpasko@icloud.com>
2017-09-08 13:49:24 +0200

    ok

--- script/js.js
+++ script/js.js
@@ -1,5 +1,7 @@
-
 $(document).ready(function(){
-    $('body').hide().fadeIn(5000);
-    $('body').css('background','linear-gradient(-90deg, #9303EF, #F5008B)')
-
+   
+    $("#move_up").click( function(){
+        $("#change_me").css({"color":"red"});
+    });
+    
+});
